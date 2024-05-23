d1e22f873d713413b83f47ce1b4dafb1e65c9410
POO <isaacamaya941@gmail.com>
2024-05-23 08:52:51 -0500

    Segundo

--- src/Taller23Java/Vehiculos.java
+++ src/Taller23Java/Vehiculos.java
@@ -4,4 +4,10 @@ public class Vehiculos {
     public String placa;
     public String marca;
     public String modelo;
+
+    public Vehiculos(String placa, String marca, String modelo){
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
 }
